64cb0204c941a0bdf5fef88795f98dd112e1351c
Jason Harrison <jharrison@jharrison-mbp.austin.q2technology.com>
2019-10-11 14:58:28 -0500

    Now binding stroke width across SVGs and users can configure.

--- app/components/gallery-customizer.js
+++ app/components/gallery-customizer.js
@@ -1,5 +1,17 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-	tagName: 'main'
+	tagName: 'main',
+	strokeWidth: '2',
+	color1: 'red',
+	color2: 'blue',
+	color3: 'yellow',
+	change(event) {
+		this.updateStrokeWidth(event.target.value);
+		return true;
+	},
+	updateStrokeWidth (newStrokeWidth) {
+		console.log(`IN ACTION >> CHANGING TO ${newStrokeWidth}`);
+		this.set('strokeWidth', newStrokeWidth)
+	}
 });
